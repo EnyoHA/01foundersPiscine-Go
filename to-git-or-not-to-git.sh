9e0340f5b299260f4bc146397e18e5ed9bd7ce76
EnyoHA <enyomha@gmail.com>
2021-11-01 14:34:50 +0000

    My second commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,3 @@
-curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"EnyoHA\"}}){id}}"}'
+#! /bin/bash
+
+curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"EnyoHA\"}}){id}}"}' | cut -d ":" -f4 | sed 's/}//g' | sed 's/]//g'
