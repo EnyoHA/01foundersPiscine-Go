4b761566e52d78ee73380d2445676dc94ed16fdf
EnyoHA <enyomha@gmail.com>
2021-11-02 16:33:06 +0000

    My second go program

---
+++ printreversealphabet/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	alphabet := "zyxwvutsrqponmlkjihgfedcba"
+
+	z01.PrintRune(rune(alphabet[0]))
+	z01.PrintRune(rune(alphabet[1]))
+	z01.PrintRune(rune(alphabet[2]))
+	z01.PrintRune(rune(alphabet[3]))
+	z01.PrintRune(rune(alphabet[4]))
+	z01.PrintRune(rune(alphabet[5]))
+	z01.PrintRune(rune(alphabet[6]))
+	z01.PrintRune(rune(alphabet[7]))
+	z01.PrintRune(rune(alphabet[8]))
+	z01.PrintRune(rune(alphabet[9]))
+	z01.PrintRune(rune(alphabet[10]))
+	z01.PrintRune(rune(alphabet[11]))
+	z01.PrintRune(rune(alphabet[12]))
+	z01.PrintRune(rune(alphabet[13]))
+	z01.PrintRune(rune(alphabet[14]))
+	z01.PrintRune(rune(alphabet[15]))
+	z01.PrintRune(rune(alphabet[16]))
+	z01.PrintRune(rune(alphabet[17]))
+	z01.PrintRune(rune(alphabet[18]))
+	z01.PrintRune(rune(alphabet[19]))
+	z01.PrintRune(rune(alphabet[20]))
+	z01.PrintRune(rune(alphabet[21]))
+	z01.PrintRune(rune(alphabet[22]))
+	z01.PrintRune(rune(alphabet[23]))
+	z01.PrintRune(rune(alphabet[24]))
+	z01.PrintRune(rune(alphabet[25]))
+	z01.PrintRune(rune('\n'))
+}
