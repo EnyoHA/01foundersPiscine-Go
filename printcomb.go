4dab3db65e7bb8b762721db4232dd328193ba27f
EnyoHA <enyomha@gmail.com>
2021-11-02 21:18:02 +0000

    combination numbers

---
+++ printcomb.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	a := '1'
+	b := '2'
+	for c := '0'; c <= '7'; c++ {
+		for a = c + 1; a <= '8'; a++ {
+			for b = a + 1; b <= '9'; b++ {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
+				if c < '7' {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				} else {
+					z01.PrintRune('\n')
+				}
+			}
+		}
+	}
+}
